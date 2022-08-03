13109bcdf8c66747c39ed00b7a4af6573353af7c
BrianCobianS <brian.cobian@alumnos.udg.mx>
2022-08-03 15:38:05 -0500

    pruebita

---
+++ yml.py
@@ -0,0 +1,23 @@
+def changeBat(fileDirectory, wordToFind, wordToChange, incremento):
+    with open(fileDirectory,'r') as archivo:
+        i=0
+        j=0
+        pos=[]
+        for linea in archivo:
+            if linea == wordToFind:
+                print("match")
+                pos.append(i)
+                j=j+1
+            i=i+1
+        print("posicion del cambio = ", pos)
+
+    contenido = open(fileDirectory).read().splitlines()
+    print(contenido)
+    for index in range(len(pos)):
+        print(pos[index])
+        contenido.insert(pos[index]-incremento,wordToChange)
+    f = open(fileDirectory, "w")
+    f.writelines("\n".join(contenido))
+
+changeBat("ola.txt","10.89.105.98","10.89.104.178",0)
+
