2e87dc4613049a6119cd47efa80d2035eced029a
BrianCobianS <brian.cobian@alumnos.udg.mx>
2022-08-03 16:31:38 -0500

    pruebita

--- yml.py
+++ yml.py
@@ -1,25 +1,13 @@
-def changeBat(fileDirectory, wordToFind, wordToChange, incremento):
-    with open(fileDirectory,'r') as archivo:
-        i=0
-        j=0
-        pos=[]
-        for linea in archivo:
-            linea=linea.lstrip()
-            linea=linea.rstrip()
-            if linea == wordToFind:
-                print("match")
-                pos.append(i)
-                j=j+1
-            i=i+1
-        print("posicion del cambio = ", pos)
-
-    contenido = open(fileDirectory).read().splitlines()
-    for index in range(len(pos)):
-        print(pos[index])
-        contenido.insert(pos[index]-incremento,"            "+wordToChange)
-
+def controller(host,user,password):
+    contenido = open("ola.txt").read().splitlines()
+    contenido.insert(6,"            "+host)
+    contenido.remove("            host")
+    contenido.insert(9,"        ansible_user: "+user)
+    contenido.remove("        ansible_user: master")
+    contenido.insert(10,"        ansible_ssh_pass: "+password)
+    contenido.remove("        ansible_ssh_pass: m1")
     f = open('fileDirectory.yml', "w")
     f.writelines("\n".join(contenido))
 
-changeBat("ola.txt","10.89.105.98","10.89.104.178",0)
+controller("olaaaaaaa","user","pasword")
 
